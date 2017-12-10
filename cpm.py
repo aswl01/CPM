1dc02a04d81974a2cc1c4b9e23d172dd4f4cb868
aswl01 <sweilun@umich.edu>
2017-12-10 02:23:21 -0500

    Almost finish the second model

--- cpm.py
+++ cpm.py
@@ -45,20 +45,42 @@ def trained_LEEDS_PC(images, center_map, scope='PoseNet', weight_decay=0.05):
                             weights_regularizer=slim.l2_regularizer(weight_decay)):
             with slim.arg_scope([slim.max_pool2d], stride=2, padding='VALID', kernel_size=3):
                 pool_center_lower = slim.avg_pool2d(center_map, 9, 8, scope='pool_center_lower')
-                conv4_stage1 = stage_x(images, 1)
+                conv4_stage1, _ = stage_x(images, 1)
                 conv5_stage1 = slim.conv2d(conv4_stage1, 512, 9, scope='conv5_stage1')
                 conv6_stage1 = slim.conv2d(conv5_stage1, 512, 1, scope='conv6_stage1')
                 conv7_stage1 = slim.conv2d(conv6_stage1, 15, 1, scope='conv7_stage1')
-                conv4_stage2 = stage_x(images, 2)
+                conv4_stage2, pool3_stage2 = stage_x(images, 2)
                 concat_stage2 = tf.concat([conv4_stage2, conv7_stage1, pool_center_lower], 3)
+                Mconv5_stage2 = substage(concat_stage2, 2)
+                conv1_stage3 = slim.conv2d(pool3_stage2, 32, 5, scope='conv1_stage3')
+                concat_stage3 = tf.concat([conv1_stage3, Mconv5_stage2, pool_center_lower], 3)
+                Mconv5_stage3 = substage(concat_stage3, 3)
+                conv1_stage4 = slim.conv2d(pool3_stage2, 32, 5, scope='conv1_stage4')
+                concat_stage4 = tf.concat([conv1_stage4, Mconv5_stage3, pool_center_lower], 3)
+                Mconv5_stage4 = substage(concat_stage4, 4)
+                conv1_stage5 = slim.conv2d(pool3_stage2, 32, 5, scope='conv1_stage5')
+                concat_stage5 = tf.concat([conv1_stage5, Mconv5_stage4, pool_center_lower], 3)
+                Mconv5_stage5 = substage(concat_stage5, 5)
+                conv1_stage6 = slim.conv2d(pool3_stage2, 32, 5, scope='conv1_stage6')
+                concat_stage6 = tf.concat([conv1_stage6, Mconv5_stage5, pool_center_lower], 3)
+                Mconv5_stage6 = substage(concat_stage6, 6)
+                return Mconv5_stage6
+
+
+def trained_MPI(images, center_map, scope='PoseNet', weight_decay=0.05):
+    with tf.variable_scope(scope, 'PoseNet'):
+        with slim.arg_scope([slim.conv2d], stride=1, padding='SAME', activation_fn=tf.nn.relu,
+                            initizalizer=tf.truncated_normal_initializer(stddev=0.1),
+                            weights_regularizer=slim.l2_regularizer(weight_decay)):
+            with slim.arg_scope([slim.max_pool2d], stride=2, padding='VALID', kernel_size=3):
 
 
 def stage_x(net, stage):
     for i in range(3):
         net = slim.conv2d(net, 128, 9, scope='conv{}_stage{}'.format(i + 1, stage))
-        net = slim.max_pool2d(net, scope='pool{}_stage1'.format(i + 1))
+        net = slim.max_pool2d(net, scope='pool{}_stage{}'.format(i + 1, stage))
     conv4_stage = slim.conv2d(net, 32, 5, scope='conv4_stage{}'.format(stage))
-    return conv4_stage
+    return conv4_stage, net
 
 
 def substage(net, stage):
