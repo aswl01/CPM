b72a7a8073c5ce43441f1ceda5790520c2b83c13
aswl01 <sweilun@umich.edu>
2017-12-12 23:24:03 -0500

    Eval Function added

--- model/cpm.py
+++ model/cpm.py
@@ -5,16 +5,21 @@ import tensorflow.contrib.slim as slim
 def trained_person_MPI(images, scope='PersonNet', weight_decay=0.05):
     with tf.variable_scope(scope, 'PersonNet'):
         with slim.arg_scope([slim.conv2d], stride=1, padding='SAME', activation_fn=tf.nn.relu,
-                            initizalizer=tf.truncated_normal_initializer(stddev=0.1),
+                            weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
                             weights_regularizer=slim.l2_regularizer(weight_decay)):
             with slim.arg_scope([slim.max_pool2d], stride=2, padding='VALID', kernel_size=2):
-                net = slim.repeat(images, 2, slim.conv2d, 64, [3, 3], scope='conv1')
+                net = images
+                for i in range(2):
+                    net = slim.conv2d(net, 64, 3, scope='conv1_{}'.format(i + 1))
                 net = slim.max_pool2d(net, scope='maxpool1')
-                net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
+                for i in range(2):
+                    net = slim.conv2d(net, 128, 3, scope='conv2_{}'.format(i + 1))
                 net = slim.max_pool2d(net, scope='maxpool2')
-                net = slim.repeat(net, 4, slim.conv2d, 256, [3, 3], scope='conv3')
+                for i in range(4):
+                    net = slim.conv2d(net, 256, 3, scope='conv3_{}'.format(i + 1))
                 net = slim.max_pool2d(net, scope='maxpool3')
-                net = slim.repeat(net, 4, slim.conv2d, 512, [3, 3], scope='conv4')
+                for i in range(4):
+                    net = slim.conv2d(net, 512, 3, scope='conv4_{}'.format(i + 1))
                 net = slim.max_pool2d(net, scope='maxpool4')
                 conv5_1 = slim.conv2d(net, 512, 3, scope='conv5_1')
                 conv5_2_CPM = slim.conv2d(conv5_1, 128, 3, scope='conv5_2_CPM')
@@ -41,7 +46,7 @@ def trained_person_MPI(images, scope='PersonNet', weight_decay=0.05):
 def trained_LEEDS_PC(images, center_map, scope='PoseNet', weight_decay=0.05):
     with tf.variable_scope(scope, 'PoseNet'):
         with slim.arg_scope([slim.conv2d], stride=1, padding='SAME', activation_fn=tf.nn.relu,
-                            initizalizer=tf.truncated_normal_initializer(stddev=0.1),
+                            weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
                             weights_regularizer=slim.l2_regularizer(weight_decay)):
             with slim.arg_scope([slim.max_pool2d], stride=2, padding='VALID', kernel_size=3):
                 pool_center_lower = slim.avg_pool2d(center_map, 9, 8, scope='pool_center_lower')
@@ -49,38 +54,49 @@ def trained_LEEDS_PC(images, center_map, scope='PoseNet', weight_decay=0.05):
                 conv5_stage1 = slim.conv2d(conv4_stage1, 512, 9, scope='conv5_stage1')
                 conv6_stage1 = slim.conv2d(conv5_stage1, 512, 1, scope='conv6_stage1')
                 conv7_stage1 = slim.conv2d(conv6_stage1, 15, 1, scope='conv7_stage1')
+                print(conv7_stage1.shape)
                 conv4_stage2, pool3_stage2 = _stage_x(images, 2)
                 concat_stage2 = tf.concat([conv4_stage2, conv7_stage1, pool_center_lower], 3)
                 Mconv5_stage2 = _LEEDS_substage(concat_stage2, 2)
+                print(Mconv5_stage2.shape)
                 conv1_stage3 = slim.conv2d(pool3_stage2, 32, 5, scope='conv1_stage3')
                 concat_stage3 = tf.concat([conv1_stage3, Mconv5_stage2, pool_center_lower], 3)
                 Mconv5_stage3 = _LEEDS_substage(concat_stage3, 3)
+                print(Mconv5_stage3.shape)
                 conv1_stage4 = slim.conv2d(pool3_stage2, 32, 5, scope='conv1_stage4')
                 concat_stage4 = tf.concat([conv1_stage4, Mconv5_stage3, pool_center_lower], 3)
                 Mconv5_stage4 = _LEEDS_substage(concat_stage4, 4)
+                print(Mconv5_stage4.shape)
                 conv1_stage5 = slim.conv2d(pool3_stage2, 32, 5, scope='conv1_stage5')
                 concat_stage5 = tf.concat([conv1_stage5, Mconv5_stage4, pool_center_lower], 3)
                 Mconv5_stage5 = _LEEDS_substage(concat_stage5, 5)
+                print(Mconv5_stage5.shape)
                 conv1_stage6 = slim.conv2d(pool3_stage2, 32, 5, scope='conv1_stage6')
                 concat_stage6 = tf.concat([conv1_stage6, Mconv5_stage5, pool_center_lower], 3)
                 Mconv5_stage6 = _LEEDS_substage(concat_stage6, 6)
+                print(Mconv5_stage6.shape)
                 return Mconv5_stage6
 
 
 def trained_MPI(images, center_map, scope='PoseNet', weight_decay=0.05):
     with tf.variable_scope(scope, 'PoseNet'):
         with slim.arg_scope([slim.conv2d], stride=1, padding='SAME', activation_fn=tf.nn.relu,
-                            initizalizer=tf.truncated_normal_initializer(stddev=0.1),
+                            weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
                             weights_regularizer=slim.l2_regularizer(weight_decay)):
             with slim.arg_scope([slim.max_pool2d], stride=2, padding='VALID', kernel_size=3):
                 pool_center_lower = slim.avg_pool2d(center_map, 9, 8, scope='pool_center_lower')
-                net = slim.repeat(images, 2, slim.conv2d, 64, [3, 3], scope='conv1')
+                net = images
+                for i in range(2):
+                    net = slim.conv2d(net, 64, 3, scope='conv1_{}'.format(i + 1))
                 net = slim.max_pool2d(net, scope='maxpool1')
-                net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
+                for i in range(2):
+                    net = slim.conv2d(net, 128, 3, scope='conv2_{}'.format(i + 1))
                 net = slim.max_pool2d(net, scope='maxpool2')
-                net = slim.repeat(net, 4, slim.conv2d, 256, [3, 3], scope='conv3')
+                for i in range(4):
+                    net = slim.conv2d(net, 256, 3, scope='conv3_{}'.format(i + 1))
                 net = slim.max_pool2d(net, scope='maxpool3')
-                net = slim.repeat(net, 2, slim.conv2d, 512, [3, 3], scope='conv4')
+                for i in range(2):
+                    net = slim.conv2d(net, 512, 3, scope='conv4_{}'.format(i + 1))
                 for i in range(3, 7):
                     net = slim.conv2d(net, 256, 3, scope='conv4_{}_CPM'.format(i))
                 conv4_7_CPM = slim.conv2d(net, 128, 3, scope='conv4_7_CPM')
@@ -98,6 +114,7 @@ def trained_MPI(images, center_map, scope='PoseNet', weight_decay=0.05):
                 Mconv7_stage6 = _MPI_substage(concat_stage6, 6)
                 return Mconv7_stage6
 
+
 def _stage_x(net, stage):
     for i in range(3):
         net = slim.conv2d(net, 128, 9, scope='conv{}_stage{}'.format(i + 1, stage))
@@ -108,7 +125,7 @@ def _stage_x(net, stage):
 
 def _LEEDS_substage(net, stage):
     for i in range(3):
-        net = slim.conv2d(net, 128, 11, scope='Mconv{}_stage_{}'.format(i + 1, stage))
+        net = slim.conv2d(net, 128, 11, scope='Mconv{}_stage{}'.format(i + 1, stage))
     Mconv4_stage = slim.conv2d(net, 128, 1, scope='Mconv4_stage{}'.format(stage))
     Mconv5_stage = slim.conv2d(Mconv4_stage, 15, 1, scope='Mconv5_stage{}'.format(stage), activation_fn=None)
     return Mconv5_stage
@@ -116,7 +133,9 @@ def _LEEDS_substage(net, stage):
 
 def _MPI_substage(net, stage):
     for i in range(5):
-        net = slim.conv2d(net, 128, 7, scope='Mconv{}_stage_{}'.format(i + 1, stage))
+        net = slim.conv2d(net, 128, 7, scope='Mconv{}_stage{}'.format(i + 1, stage))
     Mconv6_stage = slim.conv2d(net, 128, 1, scope='Mconv4_stage{}'.format(stage))
     Mconv7_stage = slim.conv2d(Mconv6_stage, 15, 1, scope='Mconv5_stage{}'.format(stage), activation_fn=None)
     return Mconv7_stage
+
+
