d2da56138e8c28f02db8b4aa711646697a59984e
aswl01 <sweilun@umich.edu>
2017-12-17 00:39:53 -0500

    Training function

--- model/cpm_network.py
+++ model/cpm_network.py
@@ -2,90 +2,43 @@ import numpy as np
 import tensorflow as tf
 from cpm import trained_LEEDS_PC
 import numpy as np
-import scipy.io as sio
 
 
 class CPM_NETWORK(object):
-    def __init__(self, imageN, imageH, imageW, num_stage=6, epoch_size=1, batch_size=1, weight_decay=0.05,
-                 random_crop=True, random_flip=True,
-                 random_contrast=True, random_rotate=True):
+    def __init__(self, pose_image_in, pose_centermap_in, labels_placeholder, batch_size, num_stage=6, weight_decay=0.05, learning_rate=10e-5):
         # define placeholder for the input image
-        self.batch_size_placeholder = tf.placeholder(tf.int32, name='batch_size')
-        self.pose_image_in = tf.placeholder(tf.float32, shape=(None, imageN, imageH, imageW, 3), name='pose_image_in')
-        self.pose_centermap_in = tf.placeholder(tf.float32, shape=(None, imageN, imageH, imageW, 1),
-                                                name='pose_centermap_in')
-        self.labels_placeholder = tf.placeholder(tf.float32, shape=(None, 3, 14), name='labels')
+        self.stage_loss = 0
+        self.total_loss = 0
+        self.batch_size = batch_size
+        self.stage_loss = [0] * num_stage
+        self.learning_rate = learning_rate
+        self.pose_image_in = pose_image_in
+        self.pose_centermap_in = pose_centermap_in
+        self.labels_placeholder = labels_placeholder
+        self.stages = num_stage
         # inference
-        self.pose_image_out, endpoint = trained_LEEDS_PC(self.pose_image_in, self.pose_centermap_in,
-                                                         weight_decay=weight_decay)
-        # calculate the loss
-        self.stage_loss = loss_func(endpoint, self.labels_placeholder)
-        tf.summary.scalar('stage_loss', self.stage_loss)
-        regularization_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-        self.total_loss = tf.add_n([self.stage_loss] + regularization_losses, name='total_loss')
+        self.pose_image_out, self.stage_heatmap = trained_LEEDS_PC(pose_image_in, pose_centermap_in,
+                                                                   weight_decay=weight_decay)
 
+    def build_loss(self):
+        self.total_loss = 0
 
-def ideal_addGaussian(x, y):
-    sigma = 21.0
+        for stage in range(self.stages):
+            with tf.variable_scope('stage' + str(stage + 1) + '_loss'):
+                self.stage_loss[stage] = tf.nn.l2_loss(self.stage_heatmap[stage] - self.labels_placeholder,
+                                                       name='l2_loss') / self.batch_size
+            tf.summary.scalar('stage' + str(stage + 1) + '_loss', self.stage_loss[stage])
 
-    xx = np.linspace(1.0, float(376), 376)
-    yy = np.linspace(1.0, float(656), 656)
+        with tf.variable_scope('total_loss'):
+            for stage in range(self.stages):
+                self.total_loss += self.stage_loss[stage]
+            tf.summary.scalar('total loss', self.total_loss)
 
-    X, Y = np.meshgrid(xx, yy)
-    X = X - x
-    Y = Y - y
+        with tf.variable_scope('train'):
+            self.global_step = tf.contrib.framework.get_or_creat_global_step()
 
-    D2 = np.power(X, 2) + np.power(Y, 2)
-
-    Exponent = D2 * (1 / sigma) * (1 / sigma) * 0.5 * (-1)
-    label_matrix = np.exp(Exponent)
-    return label_matrix
-
-
-'''
-Args:
-    stageOut: list of stage output matrix
-    index: which picture you used for this train iteration in 2000 dataset pictures
-    height: trainpic height pixels
-    width: trainpic width pixels
-
-Returns:
-    loss value
-'''
-
-
-# height width
-# stageOut batchsize X stage X 46 X 46 X 15
-# index batchsize X 3 X 14
-def loss_func(stageOut, label):
-    # Path for dataset
-    res = 0.0
-    for j in range(len(stageOut)):
-
-        coordinate_list = label[j, :, :]
-        matrix_list = []
-        for i in range(14):
-            matrix_list.append(ideal_addGaussian(coordinate_list[0][i], coordinate_list[1][i]))
-
-        ideal_matrix = matrix_list[0]
-        for i in range(13):
-            ideal_matrix = np.dstack([ideal_matrix, matrix_list[i + 1]])
-
-        ideal_matrix = np.dstack([ideal_matrix, np.zeros((376, 656))])
-
-        # reshape to 46 X 46
-        ideal_matrix = np.reshape(ideal_matrix, (46, 46, 15))
-
-        sum2 = 0.0
-
-        for i in range(6):
-            stageOutMatrix = stageOut[j, i, :, :, :]
-            tmpMatrix = stageOutMatrix - ideal_matrix
-            afterNormMatrix = np.linalg.norm(tmpMatrix, axis=2)
-            afterNormMatrix = np.power(afterNormMatrix, 2)
-            sum1 = np.sum(afterNormMatrix, axis=1)
-            sum2 += np.sum(sum1, axis=0)
-
-        res += sum2
-
-    return res
+            self.train_op = tf.contrib.layers.optimize_loss(loss=self.total_loss,
+                                                            global_step=self.global_step,
+                                                            learning_rate=self.learning_rate,
+                                                            optimizer='Adam')
+        self.merged_summary = tf.summary.merge_all()
--- train.py
+++ train.py
@@ -3,11 +3,7 @@ import codecs
 import os
 import sys
 import time
-from datetime import datetime
 import tensorflow as tf
-from sklearn.cross_validation import train_test_split
-from tensorflow.contrib import learn
-from model import cpm
 from data_utils import *
 import numpy as np
 from model.cpm_network import CPM_NETWORK
@@ -15,14 +11,31 @@ import scipy.io as sio
 
 
 def main(args):
-    if args.pretrained_model:
-        pretrained_model = tf.train.latest_checkpoint(args.pretrained_model)
-        print('Pre-trained model: %s' % os.path.expanduser(pretrained_model))
+    if args.model_path:
+        pose_net_path = os.path.join(args.model_path, 'pose_net.ckpt')
+        print('pretrained model for pose detector' + pose_net_path)
 
     matfn = args.label_file
     data = sio.loadmat(matfn)
-
+    # 3 * 14 * 2000
     data = data['joints']
+
+    # TODO: implement to get batch training data
+    # batch_x, batch_c, batch_y, batch_x_orig = tf_utils.read_batch_cpm(FLAGS.tfr_data_files, FLAGS.input_size,
+    #                                                                   FLAGS.heatmap_size, FLAGS.num_of_joints,
+    #                                                                   FLAGS.center_radius, FLAGS.batch_size)
+
+
+    # Building Graphs
+    pose_image_in = tf.placeholder(tf.float32, shape=(None, 16, 376, 376, 15), name='pose_image_in')
+    pose_centermap_in = tf.placeholder(tf.float32, shape=(None, 16, 376, 376, 1), name='pose_centermap_in')
+    labels_placeholder = tf.placeholder(tf.float32, shape=(None, 46, 46, 15), name='gt_heatmap_placeholder')
+
+    cpm = CPM_NETWORK(pose_image_in, pose_centermap_in, labels_placeholder, batch_size=args.batch_size,
+                      weight_decay=args.l2_reg_lambda, learning_rate=args.learning_rate)
+    cpm.build_loss()
+    print('Finish building the model')
+
     with tf.Graph().as_default():
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_memory_fraction)
         session_conf = tf.ConfigProto(
@@ -32,47 +45,113 @@ def main(args):
         )
         sess = tf.Session(config=session_conf)
         with sess.as_default():
-            network = CPM_NETWORK()
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(coord=coord)
             # Define Training procedure
             global_step = tf.Variable(0, name="global_step", trainable=False)
-            optimizer = tf.train.AdamOptimizer(1e-5)
-            grads_and_vars = optimizer.compute_gradients(network.loss)
-            train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+
+            timestamp = str(int(time.time()))
+            out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
+            print("Writing to {}\n".format(out_dir))
+
+            # Train Summaries
+            train_summary_dir = os.path.join(out_dir, "summaries", "train")
+            train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
+
+            saver = tf.train.Saver(tf.trainable_variables(), max_to_keep=args.num_checkpoints)
+            sess.run(tf.global_variables_initializer())
+
+            checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
+            checkpoint_prefix = os.path.join(checkpoint_dir, "model")
+
+            if args.model_path:
+                print('Restoring the latest pretrained model: %s' % pose_net_path)
+                saver.restore(sess, pose_net_path)
+
+            while global_step != args.max_iteration:
+                batch_x_np, batch_y_np, batch_c_np = sess.run([batch_x,
+                                                               batch_y,
+                                                               batch_c])
+
+                gt_heatmap_np = make_gaussian_batch(batch_y_np)
+
+                feed_dict = {pose_image_in: batch_x_np,
+                             pose_centermap_in: batch_c_np,
+                             labels_placeholder: gt_heatmap_np}
+
+                stage_losses_np, total_loss_np, _, summary, current_lr, \
+                stage_heatmap_np, global_step = sess.run([cpm.stage_loss,
+                                                          cpm.total_loss,
+                                                          cpm.train_op,
+                                                          cpm.merged_summary,
+                                                          cpm.learning_rate,
+                                                          cpm.stage_heatmap,
+                                                          cpm.global_step
+                                                          ],
+                                                         feed_dict=feed_dict)
+
+                train_summary_writer.add_summary(summary, global_step)
+
+                print('##========Iter {:>6d}========##'.format(global_step))
+                for stage_num in range(args.stages):
+                    print('Stage {} loss: {:>.3f}'.format(stage_num + 1, stage_losses_np[stage_num]))
+
+                print('Total loss: {:>.3f}\n\n'.format(total_loss_np))
+
+                if global_step % args.checkpoint_every == 0:
+                    path = saver.save(sess, save_path=checkpoint_prefix, global_step=global_step)
+                    print("Saved model checkpoint to {}\n".format(path))
+    coord.request_stop()
+    coord.join(threads)
+
+    print('Training done.')
+
+
+def make_gaussian_batch(labels):
+    batch_datum = np.zeros(shape=(labels.shape[0], 46, 46, 15))
+    stride_x = 376 // 46
+    stride_y = 656 // 46
+
+    for data_num in range(labels.shape[0]):
+        for joint_num in range(labels.shape[2]):
+            position = labels[data_num, :, joint_num]
+            x = np.arange(0, 46, 1, float)
+            y = x[:, np.newaxis]
+
+            x0 = position[0]
+            y0 = position[1]
+
+            batch_datum[data_num, :, :, joint_num] = np.exp(
+                -((x * stride_x - x0) ** 2 + (y * stride_y - y0) ** 2) / 2.0 / 3 / 3)
+        batch_datum[data_num, :, :, 14] = np.ones((46, 46)) - np.amax(batch_datum[data_num, :, :, 0:14], axis=2)
+
+    return batch_datum
 
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
 
     # Data loading params
-    parser.add_argument('--dev_sample_percentage', type=float,
-                        help='Percentage of the training data to use for validation', default=0.01)
-    parser.add_argument('--pretrained_model', type=str,
+    parser.add_argument('--model_path', type=str,
                         help='Load a pretrained model before training starts.')
     parser.add_argument('--gpu_memory_fraction', type=float,
                         help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
     parser.add_argument('--label_file', type=str, help='Path to the .mat file')
+    parser.add_argument('--log_dir', type=str, help='log directory to save the summary')
 
     # Model Hyperparameters
-    parser.add_argument('--embedding_dim', type=int,
-                        help='Dimensionality of character embedding (default: 128)', default=128)
-    parser.add_argument('--filter_sizes', type=str,
-                        help="Comma-separated filter sizes (default: 2,3,4,5')", default="2, 3, 4, 5, 6")
-    parser.add_argument('--num_filters', type=int,
-                        help='Number of filters per filter size (default: 128)', default=128)
-    parser.add_argument('--dropout_keep_prob', type=float,
-                        help='Dropout keep probability (default: 0.5)', default=0.5)
     parser.add_argument('--l2_reg_lambda', type=float,
                         help='L2 regularization lambda (default: 0.0)', default=0.0)
+    parser.add_argument('--learning_rate', type=float, help='Learning rate', default=0.0001)
+    parser.add_argument('--stages', type=int, help='Number of stages in our model', default=6)
 
     # Training parameters
     parser.add_argument('--batch_size', type=int,
-                        help='Batch Size (default: 64)', default=64)
-    parser.add_argument('--num_epochs', type=int,
-                        help='Number of training epochs (default: 200)', default=200)
-    parser.add_argument('--evaluate_every', type=int,
-                        help='Evaluate model on dev set after this many steps (default: 100)', default=100)
+                        help='Batch Size (default: 64)', default=32)
+    parser.add_argument('--max_iteration', type=int,
+                        help='Number of training iteration (default: 300000)', default=300000)
     parser.add_argument('--checkpoint_every', type=int,
-                        help='Save model after this many steps (default: 100)', default=100)
+                        help='Save model after this many steps (default: 100)', default=5000)
     parser.add_argument('--num_checkpoints', type=int,
                         help='Number of checkpoints to store (default: 3)', default=3)
     return parser.parse_args(argv)
